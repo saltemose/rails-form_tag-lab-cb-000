7549acffe5e507936e13b58d40b7e43bf2c8392c
Sean Altemose <sean@seanaltemose.com>
2018-10-10 15:47:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,7 +8,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new 
+  def new
   end
 
   def create
